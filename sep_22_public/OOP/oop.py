4d055fa930704e7953c0aee79ee3bf4b2fa1d1ee
Spencer Rauch <rauchs@gmail.com>
2022-08-30 13:12:44 -0700

    afternoon

--- sep_22_public/OOP/oop.py
+++ sep_22_public/OOP/oop.py
@@ -6,6 +6,7 @@
 #class method -- accesses of alters on the class level
 #static method -- somehow related to the class but doesn't access or alter the class OR an instance
 
+#these dictionaries will be used to instatiate cat objects
 cat1_data = {
     'name': 'Scar',
     'color': 'dark brown',
@@ -20,9 +21,13 @@ cat2_data = {
     'breed': 'lasagna'
 }
 
+
+#Cat Class Definition
 class Cat():
+    #class attributes
     all_cats = []
     is_cute = True
+    #constructor
     def __init__(self, cat_data, fav_toy_type, fav_toy_color, fav_toy_action):
         self.name = cat_data['name']
         self.color = cat_data['color']
@@ -31,42 +36,50 @@ class Cat():
         self.fav_toy = Toy(fav_toy_type,fav_toy_color,fav_toy_action)
         Cat.all_cats.append(self)
 
+    #string representation function -- special function that defines what we see if we print this object
     def __repr__(self):
         return f"Name: {self.name} color: {self.color} age: {self.age} breed: {self.breed}"
 
+    #instance method to print object info
     def print_info(self):
         print(f"Name: {self.name} color: {self.color} age: {self.age} breed: {self.breed}")
         print(f"My favorite toy is a {self.fav_toy.color} {self.fav_toy.type}")
         return self
 
+    #instance method to make the cat meow
     def meow(self):
         print(f"{self.name} lets out a cry: MEOOWWW")
         return self
 
+    #instance method to make the cat play
     def play(self):
         print(f"{self.name} plays with their {self.fav_toy.color} {self.fav_toy.type} and it {self.fav_toy.action}")
         return self
     
+    #Class method! Has access to the class, not any one instance
     @classmethod
     def print_all_cats(cls):
         for cat in cls.all_cats:
             # print(cat)
             cat.print_info()
-
+    #Static method -- cannot access class or instance, related functionality to the class
     @staticmethod
     def convert_to_cat_years(years):
         return years * 7
 
+#Toy class definition
 class Toy():
     def __init__(self, type, color, action):
         self.type = type
         self.color = color
         self.action = action
 
+#instantiating toys
 ball = Toy('ball','red','bounces on the floor')
 yarn = Toy('yarn','blue','rolls away')
 
 
+#instatiating cats with toy info as well
 cat1 = Cat(cat1_data, 'ball','red','bounces on the floor')
 cat2 = Cat(cat2_data, 'yarn','blue','rolls away')
 # print(cat1)
